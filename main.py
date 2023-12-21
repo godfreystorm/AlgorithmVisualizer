a5bf9589db27668c5ffb133164b7e8b9f8e13d8f
godfrey osagiede <godfrey@godfreys-MacBook-Air.local>
2023-12-21 16:49:14 -0500

    Added class to generate starting strings and pygame event loop to allow for pygame screen to pop up and for the 'X' icon to be able to selected to exit. I had typed 'event' instead of 'event.type' and this caused an infinite loop error which prevented the exit icon from working. Issues fixed.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 pygame.init()
 
-class DrawInformation:
+class DrawInformation: # Constructor for the drawing information.e.g. the width and height of the screen.
     BLACK = 0,0,0
     WHITE = 255,255,255
     GREEN = 0,255,0
@@ -10,27 +10,58 @@ class DrawInformation:
     GREY = 128,128,128
     BACKGROUND_COLOR = WHITE
 
-    SIDE_PADDING = 100 # padding on edges putting the chart/graph being sorted in the middle of the screen
-    TOP_PADDING = 150 # padding on the top of the screen
+    SIDE_PADDING = 100 # padding on edges putting the chart/graph being sorted in the middle of the screen.
+    TOP_PADDING = 150 # padding on the top of the screen.
 
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
 
-        self.window = pygame.display.pygame.display.set_mode((width, height))
+        self.window = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Sorting Algorithm Visulaizer")
-        self.et_list(lst)
+        self.set_list(lst)
 
     def set_list(self, lst):
         self.lst = lst
-        self.min_vlaue = min(lst)
-        self.max_value = max(lst)
+        self.min_val = min(lst)
+        self.max_val = max(lst)
 
         self.block_width = round((self.width - self.SIDE_PADDING) / len(lst))
-        self.block_height = round((self.height - self.TOP_PADDING) / (self.max_value - self.min_vlaue))
-        sels.start_x = round(self.SIDE_PADDING / 2)
+        self.block_height = round((self.height - self.TOP_PADDING) / (self.max_val - self.min_val))
+        self.start_x = round(self.SIDE_PADDING / 2)
 
-    
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+            
+    return lst
 
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50 # number of random numbers to generate.
+    min_val = 0 
+    max_val = 100
+
+    lst = generate_starting_list(n, min_val, max_val) # generate a list of random numbers between min_val and max_val.
+    draw_info = DrawInformation(800, 600, lst) # create a DrawInformation object to draw the chart/graph; The grarph is a visualization of the data to be sorted.
+
+    while run: # Loop for pygame to run until the user clicks the close button.
+        clock.tick(60)
+
+        pygame.display.update()
+
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: # Exit program; To allow the user to close the program with "X" icon.
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
 
 
