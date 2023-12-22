a813d255f46bbf4fdfb278f0fccba069d1c53f54
godfrey osagiede <godfrey@godfreys-MacBook-Air.local>
2023-12-22 15:40:16 -0500

    Chart to be ssorted was added. To be more precise, chart is now visible on background unsorted.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AlgorithmVisualizer
-An Algorithm visualizer made with python, using pygame for visualization.  Project started with making sorting algorithms visualized first.  
+An Algorithm visualizer made with python and pygame for visualization.  Project started with making sorting algorithms visualized first.  
 **Algorithms currently visualized are listed below in order of creation of visualizations.**
 ## Algorithms:
 * Bubble Sort.  
--- main.py
+++ main.py
@@ -18,24 +18,24 @@ class DrawInformation: # Constructor for the drawing information.e.g. the width 
     SIDE_PADDING = 100 # padding on edges putting the chart/graph being sorted in the middle of the screen.
     TOP_PADDING = 150 # padding on the top of the screen.
 
-    def __init__(self, width, height, lst):
+    def __init__(self, width, height, lst): # Constructor for the drawing information.e.g. the width and height of the screen.
         self.width = width
         self.height = height
 
-        self.window = pygame.display.set_mode((width, height))
-        pygame.display.set_caption("Sorting Algorithm Visulaizer")
-        self.set_list(lst)
+        self.window = pygame.display.set_mode((width, height)) # Set the window to the size of the screen.
+        pygame.display.set_caption("Sorting Algorithm Visulaizer") # Set the window caption.
+        self.set_list(lst) # Set the list of numbers to be sorted.
 
-    def set_list(self, lst):
+    def set_list(self, lst): # Set the list of numbers to be sorted.
         self.lst = lst
         self.min_val = min(lst)
         self.max_val = max(lst)
 
-        self.block_width = round((self.width - self.SIDE_PADDING) / len(lst))
-        self.block_height = round((self.height - self.TOP_PADDING) / (self.max_val - self.min_val))
-        self.start_x = round(self.SIDE_PADDING / 2)
+        self.block_width = round((self.width - self.SIDE_PADDING) / len(lst)) # calculate the width of each block.
+        self.block_height = round((self.height - self.TOP_PADDING) / (self.max_val - self.min_val)) 
+        self.start_x = round(self.SIDE_PADDING / 2) # calculate the x position of each block within the screen window.
 
-def generate_starting_list(n, min_val, max_val):
+def generate_starting_list(n, min_val, max_val): # Generate a list of random numbers between min_val and max_val to use as the starting point for the chart.
     lst = []
     for _ in range(n):
         val = random.randint(min_val, max_val)
@@ -43,18 +43,21 @@ def generate_starting_list(n, min_val, max_val):
             
     return lst
 
-def draw(draw_info): 
+def draw(draw_info): # Actual drawing of the chart with the given draw_info object.
     draw_info.window.fill(draw_info.BACKGROUND_COLOR) # Fill the background with background color variable allowing for easy change of background color.
+    draw_list(draw_info)
     pygame.display.update() # Update the window to show the chart/graph.
 
-def draw_list(draw_info):
-    lst = draw_info.lst
+def draw_list(draw_info): # Draw a list of random numbers between min_val and max_val, using the DrawInformation object.
+    lst = draw_info.lst # Get the list of numbers to be sorted.
 
-    for i val in enumerate(lst):
-        x = draw_info.start_x + i * draw_info.block_width
+    for i, val in enumerate(lst): # For each number in the list, draw a rectangle with the color determined by the gradient.
+        x = draw_info.start_x + i * draw_info.block_width 
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height 
         
-        color = 
+        color = draw_info.GRADIENTS[i % 3] # Get the color of the gradient.
+
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height)) # Draw a rectangle with the color determined by the color of the gradient.
 
 
 def main():
@@ -69,9 +72,9 @@ def main():
     draw_info = DrawInformation(800, 600, lst) # create a DrawInformation object to draw the chart/graph; The grarph is a visualization of the data to be sorted.
 
     while run: # Loop for pygame to run until the user clicks the close button.
-        clock.tick(60)
+        clock.tick(60) # Set the clock to 60 frames per second.
 
-        draw(draw_info) 
+        draw(draw_info) # Draw the chart
 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: # Exit program; To allow the user to close the program with "X" icon.
