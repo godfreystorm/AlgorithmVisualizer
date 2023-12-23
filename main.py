7accbad2b92bbdd791ca1875cd262cb90d15d548
godfrey osagiede <godfrey@godfreys-MacBook-Air.local>
2023-12-23 01:24:15 -0500

    Added and implemented buble sort algorithm, also added functionality to all buttons/controlls currently included except bubble sort button and insertion sort button. Code currently just runs bubble sort when space bar is it. In addition to the sorting, colors are added to allow for seeing of sorting process.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 class DrawInformation: # Constructor for the drawing information.e.g. the width and height of the screen.
@@ -34,7 +35,7 @@ class DrawInformation: # Constructor for the drawing information.e.g. the width 
         self.max_val = max(lst)
 
         self.block_width = round((self.width - self.SIDE_PADDING) / len(lst)) # calculate the width of each block.
-        self.block_height = round((self.height - self.TOP_PADDING) / (self.max_val - self.min_val)) 
+        self.block_height = math.floor((self.height - self.TOP_PADDING) / (self.max_val - self.min_val)) 
         self.start_x = round(self.SIDE_PADDING / 2) # calculate the x position of each block within the screen window.
 
 def generate_starting_list(n, min_val, max_val): # Generate a list of random numbers between min_val and max_val to use as the starting point for the chart.
@@ -57,8 +58,13 @@ def draw(draw_info): # Actual drawing of the chart with the given draw_info obje
     draw_list(draw_info)
     pygame.display.update() # Update the window to show the chart/graph.
 
-def draw_list(draw_info): # Draw a list of random numbers between min_val and max_val, using the DrawInformation object.
+def draw_list(draw_info, color_positions = {}, clear_bg = False): # Draw a list of random numbers between min_val and max_val, using the DrawInformation object.
     lst = draw_info.lst # Get the list of numbers to be sorted.
+
+    if clear_bg: # If the clear_bg variable is set to True, clear the background.
+        clear_rect = (draw_info.SIDE_PADDING // 2, draw_info.TOP_PADDING, 
+                    draw_info.width - draw_info.SIDE_PADDING, draw_info.height - draw_info.TOP_PADDING)
+        pygame.draw.rect(draw_info.window, draw_info.BACKGROUND_COLOR, clear_rect) 
 
     for i, val in enumerate(lst): # For each number in the list, draw a rectangle with the color determined by the gradient.
         x = draw_info.start_x + i * draw_info.block_width 
@@ -66,7 +72,27 @@ def draw_list(draw_info): # Draw a list of random numbers between min_val and ma
         
         color = draw_info.GRADIENTS[i % 3] # Get the color of the gradient.
 
+        if i in color_positions: # If the number is in the color_positions dictionary, use that color. This allows for user to easily see the sorting process.
+            color = color_positions[i]
+
         pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height)) # Draw a rectangle with the color determined by the color of the gradient.
+
+    if clear_bg:
+        pygame.display.update() # Update the window to show the chart/graph as it's beeing redrawn/sorted.
+
+def bubble_sort(draw_info, ascending = True): # Bubble sort algorithm.
+    lst = draw_info.lst # Get the list of numbers to be sorted.
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - i - 1):
+            num1 = lst[j]
+            num2 = lst[j + 1]
+
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                draw_list(draw_info, {j: draw_info.GREEN, j + 1: draw_info.RED}, True) # Chang the collor of the rectangles being compared for sorting to be green/red.
+                yield True # To be able to stop function and resume it. Esentially allows me to be able to use other controls like "R" Reset.
+    
+    return lst
 
 
 def main():
@@ -84,10 +110,21 @@ def main():
     sorting = False #Starts as false so that the program will not start sorting untill user tells it to.
     ascending = True 
 
+    sorting_algorithm = bubble_sort
+    sorting_algo_name = "Bubble Sort"
+    sorting_algorithm_generator = None
+
+
     while run: # Loop for pygame to run until the user clicks the close button.
         clock.tick(60) # Set the clock to 60 frames per second.
 
-        draw(draw_info) # Draw the chart
+        if sorting: 
+            try: 
+                next(sorting_algorithm_generator) #This is th next method that will be called if we are currently sorting. Will be called constatly untill we finish sorting the list and then next line will be executed.
+            except StopIteration: # This will be called if the top line dosent work, sorting will be set to false meaning the generator has finished.
+                sorting = False 
+        else:
+            draw(draw_info) # Draw the chart
 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: # Exit program; To allow the user to close the program with "X" icon.
@@ -100,11 +137,12 @@ def main():
                 lst = generate_starting_list(n, min_val, max_val) # regenerate the list of numbers/chart.
                 draw_info.set_list(lst) # Resseting on draw_info class so it updates when we are actually drawing the chart/graph.
                 sorting == False
-            elif event.key == pygame.K_ESCAPE and sorting == False: # The and added so we cant start sorting when already sorting.
-                sorting = True # The elif checks if user pressed the escape key. If they did, the program will start sorting.
+            elif event.key == pygame.K_SPACE and sorting == False: # The 'and' added so we can't start sorting when already sorting.
+                sorting = True # The elif checks if user pressed the space key. If they did, the program will start sorting.
+                sorting_algorithm_generator = sorting_algorithm(draw_info, ascending) 
             elif event.key == pygame.K_a and not sorting:
                 ascending = True
-            elif event.key == pygame.K_a and not sorting:
+            elif event.key == pygame.K_d and not sorting:
                 ascending = False
 
 
